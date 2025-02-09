e0a2830fe0157e933f394e0554353b1d1e67dc5e
Tahir <tahirunity786@gmail.com>
2025-02-09 11:20:03 +0500

    Home page update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -286,21 +286,236 @@ export default function Home() {
                 <button className={`${styles.btn} ${styles.btnPrimary} mb-3`}>E Shop</button>
               </div>
               <div className="col-lg-6">
-              <button className={`${styles.btn} ${styles.btnOutlineprimary} mb-3`}>Sign Up</button>
+                <button className={`${styles.btn} ${styles.btnOutlineprimary} mb-3`}>Sign Up</button>
               </div>
             </div>
 
           </div>
           <div className="col-lg-6">
-          <div className="card shadow-sm h-100 border border-light rounded-4 mb-3">
+            <div className="card shadow-sm h-100 border border-light rounded-4 mb-3">
               <div className="card-body d-flex justify-content-start">
-                
+
               </div>
             </div>
           </div>
         </div>
       </section>
+      <section className="container" style={{ marginBottom: "50px" }}>
+        <div className="text-center" style={{ position: "relative", marginBottom: "50px" }}>
+          <h4 style={{ fontWeight: "bold", marginBottom: "50px" }}>Why ATPL <span style={{ color: "#1A73E8" }}>Pro?</span></h4>
+          <Image
+            src={'/Images/double-lines.svg'}
+            width={178}
+            height={58}
+            alt="lines"
+            style={{ position: "absolute", top: "12px", right: "38%", zIndex: "100" }}
+          />
+          <p>ATPL Pro empowers aspiring pilots and aviation professionals with top-quality tools and resources, serving students, flight schools, <br /> and aviation authorities worldwide.</p>
+        </div>
+        <div className="row w-100 mt-4">
+          <div className="col-lg-7">
+            <div className="row w-100 mb-4">
+              <div className="col-lg-6">
+                <div className="card shadow-sm h-100 border border-light rounded-2 mb-3">
+                  <div className="card-body d-flex flex-column align-items-center">
+                    <div className={`${styles.circleItem} ${styles.circleColor}`} style={{ width: "60px", height: "60px" }} >
+                      <Image
+                        src={'/Images/tutor.svg'}
+                        width={40}
+                        height={40}
+                        alt="icon"
+                      />
+                    </div>
+                    <div className="text-center">
+                      <h6 style={{ fontWeight: "bold" }}>Trusted by Aviation Experts</h6>
+                      <p style={{ fontSize: "14px" }}>Our platform is developed in collaboration with experienced airline captains, ATPL instructors, and industry professionals, ensuring practical and reliable knowledge transfer.</p>
+                    </div>
 
+
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-6">
+                <div className="card shadow-sm h-100 border border-light rounded-2 mb-3">
+                  <div className="card-body d-flex flex-column align-items-center">
+                    <div className={`${styles.circleItem} ${styles.circleColor}`} style={{ width: "60px", height: "60px" }} >
+                      <Image
+                        src={'/Images/book-writing.svg'}
+                        width={40}
+                        height={40}
+                        alt="icon"
+                      />
+                    </div>
+                    <div className="text-center">
+                      <h6 style={{ fontWeight: "bold" }}>Global Reach</h6>
+                      <p style={{ fontSize: "14px" }}>From EASA-compliant exams in Europe to ICAO-compliant solutions for global users, we are the go-to resource for pilots worldwide.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row w-100 mb-4">
+              <div className="col-lg-6">
+                <div className="card shadow-sm h-100 border border-light rounded-2 mb-3">
+                  <div className="card-body d-flex flex-column align-items-center">
+                    <div className={`${styles.circleItem} ${styles.circleColor}`} style={{ width: "60px", height: "60px" }} >
+                      <Image
+                        src={'/Images/24-7.svg'}
+                        width={40}
+                        height={40}
+                        alt="icon"
+                      />
+                    </div>
+                    <div className="text-center">
+                      <h6 style={{ fontWeight: "bold" }}>Support for Flight Schools and Aviation Authorities</h6>
+                      <p style={{ fontSize: "14px" }}>Our platform is developed in collaboration with experienced airline captains, ATPL instructors, and industry professionals, ensuring practical and reliable knowledge transfer.</p>
+                    </div>
+
+
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-6">
+                <div className="card shadow-sm h-100 border border-light rounded-2 mb-3">
+                  <div className="card-body d-flex flex-column align-items-center">
+                    <div className={`${styles.circleItem} ${styles.circleColor}`} style={{ width: "60px", height: "60px" }} >
+                      <Image
+                        src={'/Images/24-7.svg'}
+                        width={40}
+                        height={40}
+                        alt="icon"
+                      />
+                    </div>
+                    <div className="text-center">
+                      <h6 style={{ fontWeight: "bold" }}>Innovation Features</h6>
+                      <p style={{ fontSize: "14px" }}>Dynamic Quality Score: Focus on high-probability questions based on recent exams.</p>
+                    </div>
+
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-5">
+            <div className="d-flex align-items-end h-100 w-100" style={{ position: "relative" }}>
+              <Image
+                src={'/Images/border-circle2.svg'}
+                width={50}
+                height={50}
+                alt="circle"
+                style={{ position: "absolute", top: "100px" }}
+              />
+              <Image
+                src={'/Images/border-circle1.svg'}
+                width={100}
+                height={100}
+                alt="circle"
+                style={{ position: "absolute", top: "0px", right: "80px" }}
+              />
+              <Image
+                src={'/Images/aero-plain.svg'}
+                width={100}
+                height={100}
+                alt="circle"
+                style={{ position: "absolute", top: "20px", right: "110px" }}
+              />
+              <Image
+                src={'/Images/freepik__background__322011.png'}
+                width={492}
+                height={442}
+                alt="pilots"
+                style={{ marginTop: "100px" }}
+              />
+            </div>
+          </div>
+        </div>
+
+      </section>
+      <section className={styles.bgWave} style={{ marginBottom: "50px", overflow: "hidden" }}>
+        <div className="container">
+
+          <div className="text-center" style={{ position: "relative", marginBottom: "50px" }}>
+            <h4 style={{ fontWeight: "bold" }}>How to <span style={{ color: "#1A73E8" }}>Order</span></h4>
+            <p style={{ fontSize: "14px" }}>Choose your path to aviation excellence with our tailored solutions</p>
+          </div>
+          <div className="row">
+            <div className="col-lg-4">
+              <div className="card shadow-sm border border-light rounded-2 mb-3">
+                <div className="card-body d-flex flex-column align-items-center justify-content-between">
+                  <div className={`${styles.circleItem} ${styles.circleColor} mb-3`} style={{ width: "60px", height: "60px" }} >
+                    <Image
+                      src={'/Images/business-and-trade.png'}
+                      width={35}
+                      height={35}
+                      alt="icon"
+                    />
+                  </div>
+                  <div className="text-center mb-3">
+                    <h6 style={{ fontWeight: "bold" }}>Aviation Authority</h6>
+                    <p style={{ fontSize: "14px" }}>Please contact us via e-mail for personalized assistance and product information</p>
+                  </div>
+                  <button className={`${styles.btn} ${styles.btnPrimary}`}>Email</button>
+
+
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="card shadow-sm border border-light rounded-2 mb-3">
+                <div className="card-body d-flex flex-column align-items-center justify-content-between">
+                  <div className={`${styles.circleItem} ${styles.circleColor} mb-3`} style={{ width: "60px", height: "60px" }} >
+                    <Image
+                      src={'/Images/graduation3.png'}
+                      width={35}
+                      height={35}
+                      alt="icon"
+                    />
+                  </div>
+                  <div className="text-center mb-3">
+                    <h6 style={{ fontWeight: "bold" }}>Flight School</h6>
+                    <p style={{ fontSize: "14px" }}>Please contact us via e-mail for personalized assistance and product information</p>
+                  </div>
+                  <button className={`${styles.btn} ${styles.btnPrimary}`}>Email</button>
+
+
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="card shadow-sm border border-light rounded-2 mb-3">
+                <div className="card-body d-flex flex-column align-items-center justify-content-between">
+                  <div className={`${styles.circleItem} ${styles.circleColor} mb-3`} style={{ width: "60px", height: "60px" }} >
+                    <Image
+                      src={'/Images/groupuserwhite.png'}
+                      width={35}
+                      height={35}
+                      alt="icon"
+                    />
+                  </div>
+                  <div className="text-center mb-3">
+                    <h6 style={{ fontWeight: "bold" }}>Private Customer</h6>
+                    <p style={{ fontSize: "14px" }}>Please purchase our products
+                      directly from our partners or
+                      your flight school. </p>
+                  </div>
+                  <button className={`${styles.btn} ${styles.btnPrimary}`}>DEALERS & PARTNERS</button>
+
+
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </section>
+
+      <section className="container">
+      <div className="text-center" style={{ position: "relative", marginBottom: "50px" }}>
+            <h4 style={{ fontWeight: "bold" }}>Frequently Asked <span style={{ color: "#1A73E8" }}>Order</span></h4>
+            <p style={{ fontSize: "14px" }}>Choose your path to aviation excellence with our tailored solutions</p>
+          </div>
+      </section>
     </>
   );
 }
