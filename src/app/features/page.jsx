5c7e85f349985a73517862cadbfe4dfad7efaa5d
Tahir <tahirunity786@gmail.com>
2025-02-10 10:11:16 +0500

    feature page update

--- src/app/features/page.jsx
+++ src/app/features/page.jsx
@@ -4,7 +4,20 @@ import styles from './features.module.css'
 const Page = () => {
   return (
     <>
-    <section className={styles.feature0bg}></section>
+    <section className={styles.feature0bg}>
+      <div className="container">
+        <div className="mb-5">
+
+      <h2 style={{fontSize:"2rem", fontWeight:"bold"}}>Features That Set Us Apart</h2>
+      <p style={{fontWeight:"bold"}}>Discover why ATPL Pro is the trusted choice for aviation training WorldWide</p>
+
+        </div>
+        <div className="mb-2 d-flex justify-content-start align-items-center">
+          <button className={`${styles.navButtonAeroWarning} ${styles.navBtnAro} me-4`}>Explore Feature</button>
+          <button className={`${styles.navButtonAeroOutlinePrimary} ${styles.navBtnAro}`}>Explore Feature</button>
+        </div>
+      </div>
+    </section>
 
     </>
   )
